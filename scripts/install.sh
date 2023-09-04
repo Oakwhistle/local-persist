710a9c967f374497352e2d83c1b79afa9adc7653
Angel Eduardo Robles Silva <arobles@globex.local>
2023-09-04 17:17:46 +0200

    Fix: dirname:missing operand

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,8 @@ set -e
 
 VERSION="v1.3.0"
 
-gitDir=$(realpath `dirname $BASH_SOURCE`/..)
+gitDir=/usr/local/bin/local-persist/
+#gitDir=$(realpath `dirname $BASH_SOURCE`/..)
 echo $gitDir
 
 # uname -s, uname -m
